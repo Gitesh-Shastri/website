d7cb3f0e9b0b8b617bb4585b9dcfe52ea139ca67
Ubuntu <ubuntu@ip-172-31-88-5.ec2.internal>
2019-01-19 14:53:51 +0000

    Changes in site

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const app = express()
 
 const port = process.env.PORT || 9000;
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 const pro = [
