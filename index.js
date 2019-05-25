b0a21fdeca909b84f7ae478082d642373cc6e2b8
apple <giteshshastri96@gmail.com>
2019-05-25 19:38:08 +0530

    Facebook Chat

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
-const Area = require('./models/area'); 
-const nodeoutlook = require("nodejs-nodemailer-outlook");
+const Area = require('./models/area');
+const nodeoutlook = require('nodejs-nodemailer-outlook');
 const Query = require('./models/query');
 const mongoose = require('mongoose');
 const moment = require('moment');
@@ -12,124 +12,135 @@ const port = process.env.PORT || 9000;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-const MONGODB_URI =
-  "mongodb://GiteshMedi:shastri1@ds263590.mlab.com:63590/medicento";
+const MONGODB_URI = 'mongodb://GiteshMedi:shastri1@ds263590.mlab.com:63590/medicento';
 
+mongoose.connect(MONGODB_URI);
+mongoose.Promise = global.Promise;
 
-  mongoose.connect(MONGODB_URI);
-  mongoose.Promise = global.Promise;
-
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const pro = [
-    {
-        medicine_name: 'Helow'
-    },
-    {
-        medicine_name: 'Helow'
-    },
-    {
-        medicine_name: 'Helow'
-    },
-    {
-        medicine_name: 'Helow'
-    }
+	{
+		medicine_name: 'Helow'
+	},
+	{
+		medicine_name: 'Helow'
+	},
+	{
+		medicine_name: 'Helow'
+	},
+	{
+		medicine_name: 'Helow'
+	}
 ];
 
 app.post('/search', (req, res, next) => {
-
-    key = {
-        medicine_name: req.body.key,
-        medicine_company: 'HHG',
-        price: 120,
-        composition: 'ehbwfjweb few bhuwefbew bfuhjebw fuewbfu ewbfuew bf ewf',
-        diseases: 'fieuw iufh ie fhehf ehufi heif eiuf',
-        quantity: 30,
-        uses: 'fkjbwefiweb fiew f eiufiue few iufb',
-
-    };
-    res.render('search', { pro: pro, key: key});
+	key = {
+		medicine_name: req.body.key,
+		medicine_company: 'HHG',
+		price: 120,
+		composition: 'ehbwfjweb few bhuwefbew bfuhjebw fuewbfu ewbfuew bf ewf',
+		diseases: 'fieuw iufh ie fhehf ehufi heif eiuf',
+		quantity: 30,
+		uses: 'fkjbwefiweb fiew f eiufiue few iufb'
+	};
+	res.render('search', { pro: pro, key: key });
 });
 
 app.get('/pharmacy-registration', (req, res, next) => {
-    res.render('addRetailer');
+	res.render('addRetailer');
 });
 
 app.post('/mail', (req, res, next) => {
-    console.log(req.body);
-    console.log(req.body.Phone.length);
-    console.log(req.body.message.length);
-    if(req.body.Phone.length > 9) {
-     Token.findById('5c4c1b029ec740040b8e5961')
-    .exec()
-    .then( doc => {
-        doc.code = doc.code+1;
-        doc.save();
-        var query = new Query({
-            customer_name: req.body.Name,
-            customer_email: req.body.Email,
-            customer_phone: req.body.Phone,
-            customer_message: req.body.message,
-            customer_query: req.body.category,
-            source: req.body.source,
-            token: doc.code
-        });
-        query.save();
-        console.log(query); 
-        var content="We have received a query on token Id "+doc.code;
-        var message1="Hello Team, <br/> we have received a query. Please find the details below of the concerned.<br/>Kindly do the needful at the earliest.";
-        message1+="<table style=\"border-collapse: collapse;\"><tr style=\"background-color: lightgray;\"><td style=\"border: 1px solid #ddd;padding: 8px;\"><strong>From</strong></td><td style=\"border: 1px solid #ddd;padding: 8px;\">"+req.body.Name+"</td></tr>";
-        message1+="<tr><td style=\"border: 1px solid #ddd;padding: 8px;\"><strong>EmailId</strong></td><td style=\"border: 1px solid #ddd;padding: 8px;\">"+req.body.Email+"</td></tr>";
-        message1+="<tr style=\"background-color: lightgray;\"><td style=\"border: 1px solid #ddd;padding: 8px;\"><strong>Phone</strong></td><td style=\"border: 1px solid #ddd;padding: 8px;\">"+req.body.Phone+"</td></tr>";
-        message1+="<tr><td style=\"border: 1px solid #ddd;padding: 8px;\"><strong>Category</strong></td><td style=\"border: 1px solid #ddd;padding: 8px;\">"+req.body.category+"</td></tr>";
-        message1+="<tr style=\"background-color: lightgray;\"><td style=\"border: 1px solid #ddd;padding: 8px;\"><strong>Message</strong></td><td style=\"border: 1px solid #ddd;padding: 8px;\">"+req.body.message+"</td></tr>";
-        nodeoutlook.sendEmail({
-            auth: {
-              user: "Team.medicento@outlook.com",
-              pass: "med4lyf@51"
-            },
-            from: "Team.medicento@outlook.com",
-            to: "contact.medicento@gmail.com,giteshshastri96@gmail.com",
-             subject: content,
-            html: message1
-          }); 
-        res.redirect('/');
-    })
-    .catch( err => {
-        console.log(err);
-        res.redirect('/');
-    });
-     } else {
-      console.log('invalid');
-      res.redirect('/');
-  }
+	console.log(req.body);
+	console.log(req.body.Phone.length);
+	console.log(req.body.message.length);
+	if (req.body.Phone.length > 9) {
+		Token.findById('5c4c1b029ec740040b8e5961')
+			.exec()
+			.then((doc) => {
+				doc.code = doc.code + 1;
+				doc.save();
+				var query = new Query({
+					customer_name: req.body.Name,
+					customer_email: req.body.Email,
+					customer_phone: req.body.Phone,
+					customer_message: req.body.message,
+					customer_query: req.body.category,
+					source: req.body.source,
+					token: doc.code
+				});
+				query.save();
+				console.log(query);
+				var content = 'We have received a query on token Id ' + doc.code;
+				var message1 =
+					'Hello Team, <br/> we have received a query. Please find the details below of the concerned.<br/>Kindly do the needful at the earliest.';
+				message1 +=
+					'<table style="border-collapse: collapse;"><tr style="background-color: lightgray;"><td style="border: 1px solid #ddd;padding: 8px;"><strong>From</strong></td><td style="border: 1px solid #ddd;padding: 8px;">' +
+					req.body.Name +
+					'</td></tr>';
+				message1 +=
+					'<tr><td style="border: 1px solid #ddd;padding: 8px;"><strong>EmailId</strong></td><td style="border: 1px solid #ddd;padding: 8px;">' +
+					req.body.Email +
+					'</td></tr>';
+				message1 +=
+					'<tr style="background-color: lightgray;"><td style="border: 1px solid #ddd;padding: 8px;"><strong>Phone</strong></td><td style="border: 1px solid #ddd;padding: 8px;">' +
+					req.body.Phone +
+					'</td></tr>';
+				message1 +=
+					'<tr><td style="border: 1px solid #ddd;padding: 8px;"><strong>Category</strong></td><td style="border: 1px solid #ddd;padding: 8px;">' +
+					req.body.category +
+					'</td></tr>';
+				message1 +=
+					'<tr style="background-color: lightgray;"><td style="border: 1px solid #ddd;padding: 8px;"><strong>Message</strong></td><td style="border: 1px solid #ddd;padding: 8px;">' +
+					req.body.message +
+					'</td></tr>';
+				nodeoutlook.sendEmail({
+					auth: {
+						user: 'Team.medicento@outlook.com',
+						pass: 'med4lyf@51'
+					},
+					from: 'Team.medicento@outlook.com',
+					to: 'contact.medicento@gmail.com,giteshshastri96@gmail.com',
+					subject: content,
+					html: message1
+				});
+				res.redirect('/');
+			})
+			.catch((err) => {
+				console.log(err);
+				res.redirect('/');
+			});
+	} else {
+		console.log('invalid');
+		res.redirect('/');
+	}
 });
 
 app.get('/medicento-privacy-policy', (req, res, next) => {
-    res.render('privacy_policy');
+	res.render('privacy_policy');
 });
 
 app.get('/about-us', (req, res, next) => {
-    res.render('aboutus');
+	res.render('aboutus');
 });
 
 app.get('/distributor-registration', (req, res, next) => {
-    res.render('addDistrbutor');
+	res.render('addDistrbutor');
 });
 
 app.get('/career', (req, res, next) => {
-    res.render('career');
+	res.render('career');
 });
 
 app.get('/careers', (req, res, next) => {
-    res.render('career');
+	res.render('career');
 });
 
 app.get('/', (req, res, next) => {
-    res.render('index', { pro: pro });
+	res.render('index', { pro: pro });
 });
 
 app.listen(port, function() {
-    console.log('Listening on PORT : ' + port );
+	console.log('Listening on PORT : ' + port);
 });
