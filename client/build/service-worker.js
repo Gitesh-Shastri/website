a177c43d99e8a6fb09ed1c275190ffd2fad058be
Gitesh-Shastri <giteshshastri100@gmail.com>
2020-03-16 23:46:07 +0530

    Added Changes

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c40211c6f739a3ef73d1cb006e0c17f.js"
+  "/precache-manifest.a2dbf93347e8b482c8cd45de481b6e2c.js"
 );
 
 self.addEventListener('message', (event) => {
