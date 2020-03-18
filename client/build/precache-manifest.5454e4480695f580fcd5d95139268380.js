c4e28311b9b1c56d6d10db86521b295bcb0f06d4
Gitesh-Shastri <giteshshastri100@gmail.com>
2020-03-18 18:58:37 +0530

    Added Changes

--- client/build/precache-manifest.5454e4480695f580fcd5d95139268380.js
+++ client/build/precache-manifest.5454e4480695f580fcd5d95139268380.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8120273ee0e3d531fc603beec49254cb",
+    "revision": "67c743a19414dcd44e8a9c811b89b40d",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.ff9ea1f9.chunk.css"
   },
   {
-    "revision": "35c886756ae8e065fbda",
-    "url": "/static/css/main.30467b01.chunk.css"
+    "revision": "015920ffea5460729eb5",
+    "url": "/static/css/main.ffa2d6c2.chunk.css"
   },
   {
     "revision": "28efa6272e9b25641a5a",
     "url": "/static/js/2.505f7a82.chunk.js"
   },
   {
-    "revision": "35c886756ae8e065fbda",
-    "url": "/static/js/main.a09a0878.chunk.js"
+    "revision": "015920ffea5460729eb5",
+    "url": "/static/js/main.c8e3bfa7.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
